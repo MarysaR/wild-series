7cc285d62807f6b0adb5a09c3fd86e6f6965822b
Mary <regentmarysapro@gmail>
2022-05-24 11:50:14 +0200

    Quete Symfony 4

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,17 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
+
+    .copyFiles({
+        from: './assets/pictures',
+        // optional target path, relative to the output dir
+        //to: 'pictures/[path][name].[ext]',
+        // if versioning is enabled, add the file hash too
+        to: 'pictures/[path][name].[hash:8].[ext]',
+        
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+        })
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
@@ -56,7 +67,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -72,4 +83,19 @@ Encore
     //.autoProvidejQuery()
 ;
 
-module.exports = Encore.getWebpackConfig();
+//supprime la dernière ligne
+// module.exports = Encore.getWebpackConfig();
+
+// et remplace par ceci
+const fullConfig = Encore.getWebpackConfig();
+fullConfig.devServer = {
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
+    watchFiles: {
+        paths: ['templates/**/*.html.twig'],
+    },
+};
+module.exports = fullConfig;
