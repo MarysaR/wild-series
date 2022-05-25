819c0f2f91a27bc1a6f77b75f4c8ca286f297b5f
Mary <regentmarysapro@gmail>
2022-05-25 10:22:43 +0200

    Quete Symfony5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -15,4 +15,12 @@ class ProgramController extends AbstractController
             'website' => 'Wild Series',
         ]);
     }
+
+    #[Route('/program/{id}', methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id
+        ]);
+    }
 }
